ba21e75e5715de97d168f3cc37e3a73d1d551b7c
Felipe <fhuaiquing@gmail.com>
2020-02-14 22:20:26 -0300

    need to finish styles

--- pokedex.js
+++ pokedex.js
@@ -4,17 +4,38 @@ $(document).ready(function(){
     for(var i = 1; i <=151; i++){
         var pkmn = i + ".png";
         var srce = "http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + pkmn;
-        output +="<figure class='image is-96x96 is-inline-block'><img class='"+i+"'src='" + srce + "' ></figure>"  ;
-        // console.log(output)
+        // var link_json = "https://pokeapi.co/api/v2/pokemon/"+i+"/";
+        output +="<figure id='"+i+"'class='image is-96x96 is-inline-block'><img id='pkmn_img' class='"+i+"'src='" + srce + "' ></figure>"  ;
     }
-    document.getElementById('pokemon').innerHTML = output;
+    document.getElementById('pokemon').innerHTML = output;    
 
-    $(this).click(function(){
-        $('#stats').slideToggle();
-        var link = "https://pokeapi.co/api/v2/pokemon/"+$(this).attr('class')
-        console.log(link)
-        // $.get()    
+    $(document).on('click','figure',function(){
+            var pkmn_number = $(this).attr("id");
+            // console.log(pkmn_number);
+            $('#stats').show();
+            // alert("The pokemon is: "+$(this).attr("id") );
+            var link = "https://pokeapi.co/api/v2/pokemon/"+ $(this).attr("id")+"/";
+            // console.log(link);
+            $.get(link, function(res){
+                $('div.content').html("<div><strong>"+res.species.name+"</strong></div>");
+                $('div.content').append("<img src='http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + pkmn_number + ".png' >")
+                if(res.types.length==1){
+                    $('div.content').append("<div><p>"+res.types[0].type.name+"</p></div>")
+                }else{
+                    $('div.content').append("<div><p>"+res.types[0].type.name+"</p></div>")
+                    $('div.content').append("<div><p>"+res.types[1].type.name+"</p></div>")
+                }
+                $('div.content').append("<div><p>"+res.height+"</p></div>")
+                $('div.content').append("<div><p>"+res.weight+"</p></div>")
+            },"json");
+
+            
     });
 });
 
+// $(document).ready(function(){
+//     $(document).on('click', 'figure', function(){
+//         console.log($(this).attr('id'));
+//     });
+// });
 
