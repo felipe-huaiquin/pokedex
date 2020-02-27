2efa827504642a1306bd4c17c6fb14e2a9bc60bf
Felipe <fhuaiquing@gmail.com>
2020-02-26 22:12:48 -0300

    need to center images

--- pokedex.js
+++ pokedex.js
@@ -1,3 +1,11 @@
+function Capitalization(text){
+    var capitalized = text.substring(0,1).toUpperCase()+text.substring(1);
+    return capitalized;
+    // Es como el arr.splice, string.substring(x,y) devuelve el string desde la posición x hasta y.
+    // string.susbtring(x) devuelve el string completo desde la posición x.
+};
+
+
 $(document).ready(function(){
     $('#stats').hide();
     var output = ""
@@ -17,25 +25,19 @@ $(document).ready(function(){
             var link = "https://pokeapi.co/api/v2/pokemon/"+ $(this).attr("id")+"/";
             // console.log(link);
             $.get(link, function(res){
-                $('div.content').html("<div><strong>"+res.species.name+"</strong></div>");
-                $('div.content').append("<img src='http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + pkmn_number + ".png' >")
+                $('div.content').html("<div class='has-text-centered'><strong>"+Capitalization(res.species.name)+"</strong></div>");
+                $('div.content').append("<img class='image-centered' src='http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + pkmn_number + ".png' >")
                 if(res.types.length==1){
-                    $('div.content').append("<div><p>"+res.types[0].type.name+"</p></div>")
+                    $('div.content').append("<div class='has-text-centered'><p>"+res.types[0].type.name+"</p></div>")
                 }else{
-                    $('div.content').append("<div><p>"+res.types[0].type.name+"</p></div>")
-                    $('div.content').append("<div><p>"+res.types[1].type.name+"</p></div>")
+                    $('div.content').append("<div class='has-text-centered'><p>"+res.types[0].type.name+"</p></div>")
+                    $('div.content').append("<div class='has-text-centered'><p>"+res.types[1].type.name+"</p></div>")
                 }
-                $('div.content').append("<div><p>"+res.height+"</p></div>")
-                $('div.content').append("<div><p>"+res.weight+"</p></div>")
+                $('div.content').append("<div class='has-text-centered'><p>"+res.height+"</p></div>")
+                $('div.content').append("<div class='has-text-centered'><p>"+res.weight+"</p></div>")
             },"json");
 
             
     });
 });
-
-// $(document).ready(function(){
-//     $(document).on('click', 'figure', function(){
-//         console.log($(this).attr('id'));
-//     });
-// });
 
